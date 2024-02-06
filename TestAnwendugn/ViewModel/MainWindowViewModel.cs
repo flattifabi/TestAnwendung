4de19ccf11a0d21b73a16f17ed1602ea087235a4
Fabian Flattinger <fabiflatti@gmail.com>
2024-02-06 22:09:34 +0100

    Projektdateien hinzufügen.

---
+++ TestAnwendugn/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using TestAnwendugn.Helpers;
+using TestAnwendugn.Views;
+
+namespace TestAnwendugn.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public string TestString { get; set; }
+        public Control Content { get; set; }
+        public ICommand ChangeContentToView1 { get; set; }
+        public ICommand ChangeContentToView2 { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Content = new EineAnzeige();
+
+            ChangeContentToView1 = new RelayCommand(param => OnChangeContentToView1());
+            ChangeContentToView2 = new RelayCommand(param => OnChangeContentToView2());
+        }
+
+        private void OnChangeContentToView2()
+        {
+            Content = new EineAnzeige();
+            TestString = "Test";
+            OnPropertyChanged(nameof(TestString));
+            OnPropertyChanged(nameof(Content));
+        }
+
+        private void OnChangeContentToView1()
+        {
+            Content = new ZweiteAnzeige();
+            TestString = "Bestanden";
+            OnPropertyChanged(nameof(TestString));
+            OnPropertyChanged(nameof(Content));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ TestAnwendugn/Views/EineAnzeige.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAnwendugn.Views
+{
+    /// <summary>
+    /// Interaktionslogik für EineAnzeige.xaml
+    /// </summary>
+    public partial class EineAnzeige : UserControl
+    {
+        public EineAnzeige()
+        {
+            InitializeComponent();
+        }
+    }
+}
